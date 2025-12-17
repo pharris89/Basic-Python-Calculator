ba732f8d4dcf59b359c4a6545d2f7673bacca14e
Paula Harris <120838423+pharris89@users.noreply.github.com>
2025-12-17 16:58:38 -0600

    Add basic arithmetic functions to mymathfunctions.py

---
+++ mymathfunctions.py
@@ -0,0 +1,12 @@
+def add_numbers(first, second):
+    sum = first + second
+    return sum
+def subtract_numbers(first, second):
+    difference = first - second
+    return difference
+def multiply_numbers(first, second):
+    total = first * second
+    return total
+def divide_numbers(first, second):
+    quotient = first / second
+    return quotient
